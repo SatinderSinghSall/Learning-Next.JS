024a71ee4c81faa494aa22e57d9b27f9267d1060
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-01 02:18:34 +0530

    Next.JS: Next.js SearchParams, Useing URL Query Strings in Next.js.

---
+++ app/(users)/products/page.jsx
@@ -0,0 +1,23 @@
+const Products = async ({ searchParams }) => {
+  const searchParam = await searchParams;
+
+  const category = searchParam?.category || "all";
+  const sort = searchParam?.sort || "default";
+  const page = searchParam?.page || 1;
+
+  return (
+    <div>
+      <main className="pt-24 px-4 min-h-screen bg-white">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Products Page:
+        </h1>
+        <p>
+          Showing - category: {category}, sort: {sort} & page: {page}
+        </p>
+        <p className="text-gray-600 text-lg">This is a Products page.</p>
+      </main>
+    </div>
+  );
+};
+// products?category=laptops&page=2&sort=price-asc
+export default Products;
