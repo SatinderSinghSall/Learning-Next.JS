67e9ca326b22fc15b07d0dee328d5bbfc4cc0bdb
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-02 01:24:29 +0530

    Optimized the database codebase.

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Satinder@123",
-  database: "hospital_db",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "Satinder@123",
+  database: process.env.DB_NAME || "hospital_db",
 });
 
-try {
-  const connection = await db.getConnection();
-  console.log(`✅ Database connected successfully!`);
-  connection.release();
-} catch (error) {
-  console.log(`❌ Error Connecting to Database: ${error}`);
-  process.exit(1);
+if (process.env.NODE_ENV !== "development") {
+  try {
+    const connection = await db.getConnection();
+    console.log(`✅ Database connected successfully!`);
+    connection.release();
+  } catch (error) {
+    console.log(`❌ Error Connecting to Database: ${error}`);
+    process.exit(1);
+  }
 }
