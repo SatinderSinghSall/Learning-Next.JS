5b15f5208a7ea178410d22d706a532e6695a43b8
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-03 00:28:59 +0530

    Static vs Dynamic Pages in Next.js with Live Build & MySQL Example.

---
+++ app/(users)/rendering/dynamic/page.jsx
@@ -0,0 +1,12 @@
+const Dynamic = () => {
+  return (
+    <main className="pt-24 px-4 min-h-screen bg-white">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+        Rendering - Dynamic:
+      </h1>
+      <p className="text-gray-600 text-lg">This is a rendering dynamic page.</p>
+    </main>
+  );
+};
+
+export default Dynamic;
---
+++ app/(users)/rendering/static/page.jsx
@@ -0,0 +1,12 @@
+const Static = () => {
+  return (
+    <main className="pt-24 px-4 min-h-screen bg-white">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+        Rendering - Static:
+      </h1>
+      <p className="text-gray-600 text-lg">This is a rendering static page.</p>
+    </main>
+  );
+};
+
+export default Static;
