2af1f22b6330cea7d96f3d31b842c660f32f0dfa
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-14 00:22:15 +0530

    Next.JS: generateStaticParams() in Next.js with MySQL DB | Convert Dynamic to Static Pages in Next.JS

---
+++ app/(users)/doctors/page.jsx
@@ -0,0 +1,12 @@
+const Doctors = () => {
+  return (
+    <main className="pt-24 px-4 min-h-screen bg-white">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Search Doctors:</h1>
+      <p className="text-gray-600 text-lg">
+        Search the doctor by id in the above path. Example - /doctors/id
+      </p>
+    </main>
+  );
+};
+
+export default Doctors;
