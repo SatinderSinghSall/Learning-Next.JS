48c28743d88f44591f4231d30f9ec969088fb064
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-16 23:07:29 +0530

    Next.JS: Next.js redirect() function & Server Redirects in App Router from next/navigation

--- app/(users)/contact/contact.action.jsx
+++ app/(users)/contact/contact.action.jsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "@/config/db";
+import { redirect } from "next/navigation";
 
 export const contactAction = async (previousState, formData) => {
   try {
@@ -9,9 +10,11 @@ export const contactAction = async (previousState, formData) => {
       `INSERT INTO contact_form(full_name, email, message) values (?, ?, ?)`,
       [name, email, message]
     );
-    return { success: true, message: "Form submitted successfully!" };
+    // return { success: true, message: "Form submitted successfully!" };
+    redirect("/");
   } catch (error) {
     console.log(`Error Submitting the Form: ${error}`);
+    if (error.message === "NEXT_REDIRECT") throw error;
     return { success: false, message: "Error while submitting!" };
   }
 };
